8758624428e500c5b35836abbedf20fcb0d0125c
brijesh13061994 <ur12ec055@gmail.com>
2022-03-28 22:25:14 +0530

    function to reverse a number

--- Reverse Integer/main.cc
+++ Reverse Integer/main.cc
@@ -1,15 +1,45 @@
 #include <iostream>
+using namespace std;
 
 class Solution {
+    int num;
 public:
-    int reverse(int x) {
-        
+    Solution(int x){
+        num=x;
+    }
+    int reverse() {
+        cout<<"Reversing the number";
+        int temp=num;
+        int rnum=0;
+        while(temp>0)
+        {
+            rnum=rnum*10 + temp%10;
+            temp/=10;
+
+        }
+        num=rnum;
+
+        return num;
+    }
+    public:
+    void print() {
+        cout<<"Number value is "<<num<<endl;
     }
 };
 
 
 int main()
 {   int num = 432;
+    Solution obj(num);
     std::cout<<"Reverse Integer Program"<<std::endl;
+    obj.print();
+
+    obj.reverse();
+
+    obj.print();
+
+
+
+
 
 }
