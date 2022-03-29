7cc85e6f91294635c2d43cc942bad68dad4ac44c
brijesh13061994 <ur12ec055@gmail.com>
2022-03-29 16:36:43 +0530

    bug fixes

--- String_To_Integer/main.cc
+++ String_To_Integer/main.cc
@@ -1,51 +1,66 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int myAtoi(string s) {
-        int64_t len = s.length();
-        bool NegFlag=false, NumFlag=false;
+    int myAtoi(string s)
+    {
+        int len = s.length();
+        bool NegFlag = false, SignFlag = false, NumFlag = false;
 
-        int num=0;
+        int num = 0;
 
-        for(int i=0;i<len;i++)
+        for (int i = 0; i < len; i++)
         {
-            if( (s[i]>='0' && s[i]<='9') 
-                || s[i]=='-' || s[i]=='+' || s[i]==' ') 
+            if ((s[i] >= '0' && s[i] <= '9') || s[i] == '-' || s[i] == '+' || s[i] == ' ')
             {
-                if (s[i]>='0' && s[i]<='9') 
-                {   
-                    NumFlag=true;
-                    num = num*10 + s[i]-'0';
-                }
-                else if(s[i] == '-')
-                    NegFlag=true;
-                    
-                else if(s[i]=='+')
-                    NegFlag=false;
+                if (s[i] >= '0' && s[i] <= '9')
+                {
+                    NumFlag = true;
 
+                    if (num > INT32_MAX / 10)
+                    {
+                        if (NegFlag)
+                            return INT32_MIN;
+                        else
+                            return INT32_MAX;
+                    }
+
+                    num = num * 10 + s[i] - '0';
+                }
+                else if (s[i] == '-' && !SignFlag)
+                {
+                    SignFlag = true;
+                    NegFlag = true;
+                }
+
+                else if (s[i] == '+' && !SignFlag)
+                {
+                    SignFlag = true;
+                }
+                else if ((s[i] == '-' || s[i] == '+') && SignFlag)
+                    return num;
             }
 
-            else if(NegFlag)
-                return num*(-1);
-            else if(!NegFlag)
+            else if (NegFlag)
+                return num * (-1);
+            else if (!NegFlag)
                 return num;
-
         }
-        if(NegFlag)
-            return (-1)*num;
-        else    
+        if (NegFlag)
+            return (-1) * num;
+        else
             return num;
     }
 };
 
-int main(){
+int main()
+{
     Solution myObj;
-    string str ="  h-123 heello";
-    
-    cout<<"WELCOME to String to Integer Conversion"<<endl;
-    cout<<"String "<<str<<" equals : "<<myObj.myAtoi(str)<<endl;
+    string str = "  --123 heello";
 
+    cout << "WELCOME to String to Integer Conversion" << endl;
+    cout << "String " << str << " equals : " << myObj.myAtoi(str) << endl;
 }
