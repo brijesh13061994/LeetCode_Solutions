de24b022b6276fbfe159986992507902453c9df3
brijesh13061994 <ur12ec055@gmail.com>
2022-03-29 19:19:49 +0530

    Palindrome Number :Basic skull

---
+++ Palindrome_number/main.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+    }
+};
+
+int main()
+{
+    cout<<"Welcome to checing the palindrome number"<<endl;
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 My way of doing coding and learning Git on the fly
 1. Reverse Integer
 2. String to Integer (atoi)
+3. Palindrome Number
