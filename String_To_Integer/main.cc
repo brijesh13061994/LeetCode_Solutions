a35ef1034bd718f53da3c86438d67e6cec4c2681
brijesh13061994 <ur12ec055@gmail.com>
2022-03-29 11:59:34 +0530

    ongoing

--- String_To_Integer/main.cc
+++ String_To_Integer/main.cc
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
 
+class Solution {
+public:
+    int myAtoi(string s) {
+        
+    }
+};
+
 int main(){
+    Solution myObj;
+    string str ="123";
+    
     cout<<"WELCOME to String to Integer Conversion"<<endl;
+
+    cout<<"String "<<str<<"equals : "<<myObj.myAtoi("123")<<endl;
 }
