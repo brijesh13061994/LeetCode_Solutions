dd870300d251916ce94eeaafad275dc23faa8afc
brijesh13061994 <ur12ec055@gmail.com>
2022-03-29 10:38:35 +0530

    Bug fixes for overflowing Integer

--- Reverse Integer/main.cc
+++ Reverse Integer/main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+
 class Solution {
     int num;
 public:
@@ -8,16 +9,39 @@ public:
         num=x;
     }
     int reverse() {
-        cout<<"Reversing the number";
-        int temp=num;
+        cout<<"Reversing the number"<<endl;
+        int temp,s1;   
+        bool first=false;
+
+        if(num>0)
+            temp=num;
+        else if(num<0)
+            temp=num * (-1);
+        else if(num = 0)
+            return 0;
+
         int rnum=0;
         while(temp>0)
         {
             rnum=rnum*10 + temp%10;
-            temp/=10;
 
+            if(!first)
+                s1=temp%10;
+
+
+            temp/=10;
         }
-        num=rnum;
+
+        if(s1!=(rnum%10)){
+            cout<<"Overflow Occured while Reversing the number"<<endl;
+            num=0;
+            return 0;
+        }        
+
+        if(num>0)
+            num=rnum;
+        else
+            num=rnum*(-1);
 
         return num;
     }
@@ -29,17 +53,15 @@ public:
 
 
 int main()
-{   int num = 432;
+{   int num = INT32_MAX;
     Solution obj(num);
-    std::cout<<"Reverse Integer Program"<<std::endl;
+    cout<<"Reverse Integer Program sizeof(INT) = "<<sizeof(int)<<endl;
+
+
     obj.print();
 
     obj.reverse();
 
     obj.print();
-
-
-
-
 
 }
