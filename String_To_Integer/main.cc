18b03ef0536919ceefa86a274c3c114de8976495
brijesh13061994 <ur12ec055@gmail.com>
2022-03-29 13:00:40 +0530

    added ATOI

--- String_To_Integer/main.cc
+++ String_To_Integer/main.cc
@@ -1,10 +1,19 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 
 class Solution {
 public:
     int myAtoi(string s) {
-        
+        int len = s.length();
+        int num=0;
+
+        for(int i=0;i<len;i++)
+        {
+            num =   num*10 + s[i]-'0';
+
+        }
+        return num;
     }
 };
 
@@ -14,5 +23,5 @@ int main(){
     
     cout<<"WELCOME to String to Integer Conversion"<<endl;
 
-    cout<<"String "<<str<<"equals : "<<myObj.myAtoi("123")<<endl;
+    cout<<"String "<<str<<" equals : "<<myObj.myAtoi("123")<<endl;
 }
