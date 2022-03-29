332fd3ffeeceb3168ab5ff83017742d3b792e0f4
brijesh13061994 <ur12ec055@gmail.com>
2022-03-29 16:11:09 +0530

    ATOI BugFixes

--- String_To_Integer/main.cc
+++ String_To_Integer/main.cc
@@ -5,16 +5,21 @@ using namespace std;
 class Solution {
 public:
     int myAtoi(string s) {
-        int len = s.length();
-        bool NegFlag=false;
+        int64_t len = s.length();
+        bool NegFlag=false, NumFlag=false;
+
         int num=0;
 
         for(int i=0;i<len;i++)
         {
-            if( (s[i]>='0' && s[i]<='9') || s[i]=='-' || s[i]=='+')
+            if( (s[i]>='0' && s[i]<='9') 
+                || s[i]=='-' || s[i]=='+' || s[i]==' ') 
             {
                 if (s[i]>='0' && s[i]<='9') 
+                {   
+                    NumFlag=true;
                     num = num*10 + s[i]-'0';
+                }
                 else if(s[i] == '-')
                     NegFlag=true;
                     
@@ -23,8 +28,12 @@ public:
 
             }
 
-        }
+            else if(NegFlag)
+                return num*(-1);
+            else if(!NegFlag)
+                return num;
 
+        }
         if(NegFlag)
             return (-1)*num;
         else    
@@ -34,7 +43,7 @@ public:
 
 int main(){
     Solution myObj;
-    string str ="-123";
+    string str ="  h-123 heello";
     
     cout<<"WELCOME to String to Integer Conversion"<<endl;
     cout<<"String "<<str<<" equals : "<<myObj.myAtoi(str)<<endl;
