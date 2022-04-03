7490b94abd50e05e7f0b7b3ba96753fe34c1b622
brijesh <brijesh13061994@gmail.com>
2022-04-03 21:35:47 +0530

    removed get digits , added rev Number

--- Palindrome_number/main.cc
+++ Palindrome_number/main.cc
@@ -2,21 +2,28 @@
 using namespace std;
 
 class Solution {
-    int getDigits(int x)
+    int revNum(int x)
     {
-        int count=0;
-        while(x){
+        int rev =0,digit=0;
+        while(x)
+        {
+            digit=x%10;
+            rev=rev*10 + digit;
+            x/=10;
 
         }
+        return rev;
     }
 public:
     bool isPalindrome(int x) {
-        
+        return x==revNum(x);
     }
 };
 
 int main()
 {
     Solution obj;
-    cout<<"Welcome to checing the palindrome number"<<endl;
+    cout<<"Welcome to checing the palindrome number "<<obj.isPalindrome(112211)<<endl;
+    
+    return 0;
 }
