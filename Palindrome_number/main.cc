08c98a8172172a9efb68ff4ab0c7cb195d5e4ba9
brijesh <brijesh13061994@gmail.com>
2022-04-03 21:40:35 +0530

    added comments

--- Palindrome_number/main.cc
+++ Palindrome_number/main.cc
@@ -2,6 +2,12 @@
 using namespace std;
 
 class Solution {
+    /**
+     * @brief reverses the number and returns the reversed value
+     * 
+     * @param x 
+     * @return int 
+     */
     int revNum(int x)
     {
         int rev =0,digit=0;
@@ -15,6 +21,14 @@ class Solution {
         return rev;
     }
 public:
+/**
+ * @brief checks if a number is a palindrome or not by 
+ * reversing the number and comparing it with the number
+ * 
+ * @param x 
+ * @return true 
+ * @return false 
+ */
     bool isPalindrome(int x) {
         return x==revNum(x);
     }
