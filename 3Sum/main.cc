04986b44a74da2300c10ce3bf24ec53c2397e655
brijesh <brijesh13061994@gmail.com>
2022-04-23 14:54:08 +0530

    3 sum body

--- 3Sum/main.cc
+++ 3Sum/main.cc
@@ -2,20 +2,70 @@
 #include<vector>
 using namespace std;
 
+#define max(x,y) ((x>y)?x:y)
+#define min(x,y) ((x<y)?x:y)
+
 class Solution {
+    int small,mid ,big;
+    void order_init(int a, int b,int c){
+        big = max(a,max(b,c));
+        small=min(a,min(b,c));        
+    }
+void insertNonDuplicateElement(vector<vector<int>>& ans,vector<int>& nums)
+{
+    bool isRepeat=false;
+    int i;
+    for(i=0;i<ans.size();i++)
+    {
+        if (ans[i]==nums)
+        {
+            isRepeat=true;
+        }
+    }
+    if(!isRepeat){
+        ans.insert(ans.end(),nums);
+    }
+
+}
+
 /**
  * @brief add you function here below public
  * 
  */
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
+        int i,j,k;
+        int c=0;
+        int small, mid,big;
+        vector<int> temp;
+        vector<vector<int>> answer;
+        if(nums.size()<3)
+            return answer;
+
+        for(i=0;i<nums.size()-2;i++)
+            for(j=1;j<nums.size()-1;j++)
+                for(k=2;k<nums.size();k++)
+                {
+                    if(nums[i]+nums[j]+nums[k]==0)
+                        {
+                            order_init(nums[i],nums[j],nums[k]);
+
+                            temp.insert(temp.begin(),small);
+                            temp.insert(temp.begin()+1,mid);
+                            temp.insert(temp.begin()+2,big);
+                            insertNonDuplicateElement( answer,temp );
+                            temp.clear();
+                            // answer.insert(answer.begin()+c,temp);
+                        }
+
+                }
+        return answer;
     }
 };
 
 int main()
 {
     Solution obj;
-    cout<<"Welcome to ***"<<endl;
+    cout<<"Welcome to 3 SUM"<<endl;
     return 0;
 }
